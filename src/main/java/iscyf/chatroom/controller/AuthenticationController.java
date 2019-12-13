f1d27a897e07e50cd9d51d18ebeaeda0d54856ec
754846243 <754846243@qq.com>
2019-12-13 11:01:30 +0800

    无权访问时，访问状态码401

--- src/main/java/iscyf/chatroom/controller/AuthenticationController.java
+++ src/main/java/iscyf/chatroom/controller/AuthenticationController.java
@@ -5,6 +5,9 @@ import iscyf.chatroom.vo.ResultVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author 陈雨菲
  * @description 处理未登录的情况
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     @RequestMapping("/authentication")
-    public ResultVO authentication () {
-        return ResultVOUtil.error("000", "无访问权限，请先登录");
+    public void authentication (HttpServletResponse response) throws IOException {
+        response.setStatus(401);
     }
 }
